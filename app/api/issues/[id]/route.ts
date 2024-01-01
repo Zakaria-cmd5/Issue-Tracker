ef0b4777cec511bdd63a25175746bec0e0de2a60
Zakaria Asad <zasad2587@gmail.com>
2024-01-01 03:12:15 +0200

    Building an API for edit the issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,33 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), {
+      status: 400,
+    });
+
+  const { title, description } = body;
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title,
+      description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
